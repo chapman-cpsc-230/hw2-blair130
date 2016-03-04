85aaa5f0c42289b1ab5c5f9261f3e5f79ad97592
Lucas Blair <blair130@mail.chapman.edu>
2016-03-03 23:57:44 -0800

    Added files via upload

---
+++ sum_powers.py
@@ -0,0 +1,20 @@
+import math
+
+b_str = input ("Enter a base number:")
+b = float(b_str)
+
+
+n_str = input ("Enter a natural number:")
+n = int(n_str)
+
+i = 0
+sum = 0
+
+
+while i <= n:
+    sum += pow (b, i)
+    i += 1
+
+print "The result is %d" %sum
+
+print "The second part of the problem is %d" %(pow (b, ((n+1)/(b-1))))
